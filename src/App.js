b9858833d15f619fb81ab63465eaa62d46270768
Niraj_Kumar <61028887+Nirajsah17@users.noreply.github.com>
2023-01-08 08:09:28 +0000

    setup of tailwind icon and react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data/home.json';
+import { FaHeart } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div>
+        <h1 className="font-bold">
+          Hello world! {data.Nav.name}
+        </h1>
+        <FaHeart />
+      </div>
+
+    );
+  }
 
 export default App;
